680c3163c486bb97c26bb45d1f39292799ec0b4b
angelqtypie <angeljustineballaso07@gmail.com>
2023-11-09 13:58:57 -0800

    prutaaasseu

---
+++ src/oop/Flavor.java
@@ -0,0 +1,11 @@
+package oop;
+
+
+public class Flavor extends Fruits {
+    Flavor(String name){
+    super(name);
+}
+    void fall(){
+    System.out.println(name +" gitagak ang prutas");
+    }
+}
---
+++ src/oop/Main.java
@@ -0,0 +1,12 @@
+package oop;
+
+public class Main {
+  public static void main(String[] args){
+  
+      Fruits myFave = new Fruits("Angel");
+      
+      myFave.eat();
+      myFave.peel();
+      
+  }  
+}
